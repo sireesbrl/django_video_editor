b19dcd23b3899e24061efd22f2631e4db9aa5097
Sirees <sireesbrl@gmail.com>
2024-12-23 20:37:44 +0545

    update models

--- video_editor/models.py
+++ video_editor/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
-
 class Video(models.Model):
     title = models.CharField(max_length=255)
     original_video = models.FileField(upload_to='videos/')
-    processed_video = models.CharField(max_length=255, blank=True, null=True)
+    original_audio = models.FileField(upload_to='audio/', blank=True, null=True)  # Optional audio field
+    processed_video = models.CharField(max_length=255, blank=True, null=True)  # Path to processed video
+    text_content = models.CharField(max_length=255, blank=True, null=True)  # Text content for overlay
+    text_position = models.CharField(max_length=255, blank=True, null=True, default="center,center")  # Position of text
 
     def __str__(self):
         return self.title
